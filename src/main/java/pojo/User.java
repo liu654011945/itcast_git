b8354162cef03ca440b09f0bba8a2c4b6c9b7123
Administrator <111@163.com>
2019-04-23 15:58:46 +0800

    firstcommit

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,32 @@
+package pojo;
+
+import java.io.Serializable;
+
+/**
+ * 描述
+ *
+ * @author 三国的包子
+ * @version 1.0
+ * @package pojo *
+ * @since 1.0
+ */
+public class User implements Serializable {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
